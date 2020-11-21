78fddfc98a231714d8a58d571cee8268a0d779bd
Yuri <Yuribtr@users.noreply.github.com>
2020-11-21 12:07:46 +0300

    Added SELECT queries as per Task 2

--- main.py
+++ main.py
@@ -94,3 +94,35 @@ res = 0
 for item in DATA:
     res += connection.execute(query.format(item['collection'], item['track'])).rowcount
 print(f'Inserted {res} connections.')
+
+print('\nDatabase ready, let\'s have some fun...')
+
+print('\nAll albums from 2018:')
+query = read_query('select-album-by-year.sql')
+res = connection.execute(query.format(2018))
+print(*res, sep='\n')
+
+print('\nLongest track:')
+query = read_query('select-longest-track.sql')
+res = connection.execute(query)
+print(*res, sep='\n')
+
+print('\nTracks with length not less 3.5min:')
+query = read_query('select-tracks-over-length.sql')
+res = connection.execute(query.format(310))
+print(*res, sep='\n')
+
+print('\nCollections between 2018 and 2020 years (inclusive):')
+query = read_query('select-collections-by-year.sql')
+res = connection.execute(query.format(2018, 2020))
+print(*res, sep='\n')
+
+print('\nMusicians with name that contains not more 1 word:')
+query = read_query('select-musicians-by-name.sql')
+res = connection.execute(query)
+print(*res, sep='\n')
+
+print('\nTracks that contains word "me" in name:')
+query = read_query('select-tracks-by-name.sql')
+res = connection.execute(query.format('me'))
+print(*res, sep='\n')
